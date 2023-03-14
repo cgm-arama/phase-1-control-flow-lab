2099ca00427c4e6a0b97d92226085037fe4b8764
Eric <eric.arama@student.moringaschool.com>
2023-03-14 09:11:43 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+  if(ride <= 400){
+   return 'This one is on me!'
+ }else if(ride <= 2499){
+   return 'I will gladly take your thirty bucks.'
+ }else if(ride => 2500){
+   return 'No can do.'
+ }
+ }
+ 
+ function ternaryCheckCity(city){
+   if(city === "NYC"){
+     return 'Ok, sounds good.'
+   }else{
+     return 'No go.'
+   }
+ }
+ 
+ console.log(ternaryCheckCity())
+ 
+ function switchOnCharmFromTip(tip){
+   if(tip === 'generous'){
+     return 'Thank you so much.'
+   }else if(tip === 'not as generous'){
+     return 'Thank you.'
+   }else{
+     return 'Bye.'
+   }
+ }
+ 
